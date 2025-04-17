991d60646f3504d9ba9e72060d9e108fe907855f
aman-bits-pilani <202217b3005@wilp.bits-pilani.ac.in>
2025-04-17 10:39:45 +0530

    Initial commit

---
+++ gold_price_script.py
@@ -0,0 +1,41 @@
+import asyncio
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from telegram import Bot
+
+# Replace with your bot token and chat ID
+TELEGRAM_TOKEN = "8176364398:AAE6gXKfq8pPqySKoTrPIs5zHZ4O0hvmP-8"
+CHAT_ID = "1771038704"  # Your chat ID
+
+async def send_to_telegram(price):
+    bot = Bot(token=TELEGRAM_TOKEN)
+    await bot.send_message(chat_id=CHAT_ID, text=f"Today's Malabar 22K Gold Price: {price}")
+
+def get_gold_price():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
+    try:
+        driver.get("https://www.malabargoldanddiamonds.com/goldprice")
+        time.sleep(5)  # Wait for the page to load
+
+        # Locate the element that contains the gold price
+        price_element = driver.find_element(By.XPATH, "(//li/span[@class='price 22kt-price'])")  # XPath to the price
+        price = price_element.text.strip()
+        print(f"Gold Price: {price}")
+        asyncio.run(send_to_telegram(price))  # Send the price to Telegram
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    get_gold_price()
